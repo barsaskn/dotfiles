8356060f45d26fb163f23ce0fb604f186c08bb81
barsaskn <barisaskin09@gmail.com>
2024-06-26 15:11:38 +0300

    cmp.lua updated

--- private_dot_config/nvim/lua/configs/cmp.lua
+++ private_dot_config/nvim/lua/configs/cmp.lua
@@ -1,9 +1,9 @@
 local cmp = require("cmp")
 
-local options = function(_, opts)
-  
-  opts.mapping["<Down>"] = cmp.mapping.select_next_item()
-  opts.mapping["<Up>"] = cmp.mapping.select_prev_item()
-end
-
+local options = {
+  mapping = {
+    ["<Down>"] = cmp.mapping.select_next_item(),
+    ["<Up>"] = cmp.mapping.select_prev_item(),
+  }
+}
 return options
