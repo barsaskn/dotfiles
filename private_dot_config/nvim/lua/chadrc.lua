4e6df1bbd75a910d61a05c1ee2c1fcde19961e01
barsaskn <barisaskin09@gmail.com>
2024-06-27 08:54:13 +0300

    Nvim dashboard quit button added

--- private_dot_config/nvim/lua/chadrc.lua
+++ private_dot_config/nvim/lua/chadrc.lua
@@ -7,7 +7,15 @@ local M = {}
 M.ui = {
 	theme = "doomchad",
   nvdash = {      -- Dashboard
-    load_on_startup = true
+    load_on_startup = true,
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+      { "  Quit", "", "qall" },
+    }
   },
   statusline = {
     theme = "vscode_colored",
