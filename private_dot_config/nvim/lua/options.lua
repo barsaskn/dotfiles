a7496fb26e6f1353360e876daf24069fb2fff6e5
barsaskn <barisaskin09@gmail.com>
2024-07-04 16:29:14 +0300

    Nvim scrolloff option set

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -12,11 +12,13 @@ g.autoformat = false
 
 opt.fixendofline = false
 opt.relativenumber = false
-opt.whichwrap:append('>,<,h,l')
+opt.whichwrap:append ">,<,h,l"
 opt.wrap = false
 
 opt.encoding = "utf-8"
 opt.fileencoding = "utf-8"
-opt.guicursor="n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+opt.guicursor =
+  "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+opt.scrolloff = 10
 
 vim.scriptencoding = "utf-8"
