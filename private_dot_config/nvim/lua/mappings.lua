a1d84f040b1c621dbeb5344fca12ce3982d31d52
barsaskn <barisaskin09@gmail.com>
2024-07-18 15:35:40 +0300

    Nvim whichkey updated and lazygit plugin added

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -1,22 +1,25 @@
 require "nvchad.mappings"
 
-local wk = require("which-key")
+local wk = require "which-key"
 local map = vim.keymap.set
+local del = vim.keymap.del
 
-wk.register({
-  ["<leader>f"] = {
-    name = "Find",
-  },
-})
+wk.add { 
+  {
+    { "<leader>f", group = "Find" },
+  }
+}
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<Leader>gg", "<cmd> Glow <cr>", { desc = "Show file preview" })
 map("n", "<Leader>q", "<cmd> q! <cr>", { desc = "Quit" })
-map("n", "<Leader>ft", "<cmd> TodoTelescope <cr>", { desc = "Telescope Todo grep"})
+map("n", "<Leader>ft", "<cmd> TodoTelescope <cr>", { desc = "Telescope Todo grep" })
 
 for i = 1, 9, 1 do
   map("n", string.format("<A-%s>", i), function()
     vim.api.nvim_set_current_buf(vim.t.bufs[i])
   end)
 end
+
+del("n", "<leader>ma")
--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -71,6 +71,7 @@ return {
   {
     "folke/which-key.nvim",
     opts = {
+      preset = "helix",
       icons = {
         group = "  ",
       },
@@ -120,6 +121,25 @@ return {
     cmd = { "Flog", "Flogsplit", "Floggit" },
     dependencies = {
       "tpope/vim-fugitive",
+    },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     },
   },
 }
