df432eaaf0db58385ade0b5d62c985ccc4127780
barsaskn <barisaskin09@gmail.com>
2024-07-18 20:55:44 +0300

    Nvim keymaps formed

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -7,14 +7,18 @@ local del = vim.keymap.del
 wk.add { 
   {
     { "<leader>f", group = "Find" },
+    { "<leader>g", group = "Git" },
   }
 }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<Leader>gg", "<cmd> Glow <cr>", { desc = "Show file preview" })
+map("n", "<Leader>p", "<cmd> Glow <cr>", { desc = "Show file preview" })
 map("n", "<Leader>q", "<cmd> q! <cr>", { desc = "Quit" })
 map("n", "<Leader>ft", "<cmd> TodoTelescope <cr>", { desc = "Telescope Todo grep" })
+map("n", "<Leader>gg", "<cmd> LazyGit <cr>", { desc = "LazyGit" })
+map("n", "<Leader>T", "<cmd>Telescope themes<CR>", { desc = "Themes" })
+map("n", "<leader>H", "<cmd>NvCheatsheet<CR>", { desc = "toggle nvcheatsheet" })
 
 for i = 1, 9, 1 do
   map("n", string.format("<A-%s>", i), function()
@@ -23,3 +27,11 @@ for i = 1, 9, 1 do
 end
 
 del("n", "<leader>ma")
+del("n", "<leader>n")
+del("n", "<leader>cm")
+del("n", "<leader>cc")
+del("n", "<leader>ds")
+del("n", "<leader>pt")
+del("n", "<leader>rn")
+del("n", "<leader>th")
+del("n", "<leader>ch")
