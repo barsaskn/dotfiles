a3bc169fdae350ed3f5c0384befb9d221a3cbf99
barsaskn <barisaskin09@gmail.com>
2024-07-22 14:34:44 +0300

    Nvim clangd initialized

--- private_dot_config/nvim/lua/configs/lspconfig.lua
+++ private_dot_config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "pyright", "bashls", "cmake", "lua_ls", "gopls", "rust_analyzer", "docker_compose_language_service" }
+local servers = { "html", "cssls", "pyright", "bashls", "cmake", "lua_ls", "gopls", "rust_analyzer", "docker_compose_language_service", "clangd" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
