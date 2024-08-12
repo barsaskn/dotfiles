782f03edce88300ec4f8971ab7d6ddb7ee334e28
barsaskn <barisaskin09@gmail.com>
2024-08-12 16:43:39 +0300

    Nvim outline plugin added

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -182,4 +182,24 @@ return {
     },
     event = "ModeChanged",
   },
+  {
+    "hedyhli/outline.nvim",
+    cmd = {
+      "Outline",
+      "OutlineFocus",
+      "OutlineFocusOutline",
+      "OutlineFocusCode",
+      "OutlineStatus",
+      "OutlineFollow",
+      "OutlineRefresh",
+    },
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+
+      require("outline").setup {
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
+  },
 }
