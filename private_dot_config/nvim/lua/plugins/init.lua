6ee0fa5897933798bf10fc8c7e7ced4a93f276d8
barsaskn <barisaskin09@gmail.com>
2024-09-08 12:16:24 +0300

    Nvim file manager changed to the neo-tree

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -27,8 +27,20 @@ return {
     cmd = "Glow",
   },
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    cmd = "Neotree",
+  },
+  {
     "nvim-tree/nvim-tree.lua",
     config = require "configs.nvimtree",
+    enabled = false,
   },
   {
     "williamboman/mason.nvim",
