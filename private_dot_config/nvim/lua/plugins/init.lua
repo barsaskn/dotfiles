cd3421acb046ceae295f860f46515a7512a68047
barsaskn <barisaskin09@gmail.com>
2024-09-10 16:10:57 +0300

    Nvim barbar.nvim added

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -234,4 +234,24 @@ return {
       }
     end,
   },
+  {
+    "romgrk/barbar.nvim",
+    dependencies = {
+      "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+      "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+    },
+    event = "BufEnter",
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      animation = true,
+      sidebar_filetypes = {
+        ["neo-tree"] = { event = "BufWinLeave" },
+      },
+      minimum_padding = 0,
+    },
+    version = "^1.0.0", -- optional: only update when a new 1.x version is released
+  },
 }
